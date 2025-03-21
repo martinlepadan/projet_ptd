970aa6e8277142db0f056d694f2ca3f6faf9d942
Martin <martin.lepadanc@gmail.com>
2025-03-21 12:29:27 +0100

    Modification de get_df_from_query

--- app/functions.py
+++ app/functions.py
@@ -6,10 +6,15 @@ DB_PATH = os.path.join(os.getcwd(), "formule_1.db")
 QUERY_PATH = os.path.join(os.getcwd(), "app", "queries.txt")
 
 
-def get_query_as_df(query, params):
+def get_query_as_df(query, params: list = None):
     connection = sqlite3.connect(DB_PATH)
+    if not isinstance(params, list) and list is not None:
+        raise TypeError('Les paramètres doivent être une liste ou None')
     try:
-        result = pd.read_sql_query(query, connection, params=[params])
+        if params is None:
+            result = pd.read_sql_query(query, connection)
+        else:
+            result = pd.read_sql_query(query, connection, params=params)
         return result
     except sqlite3.Error as e:
         print(f"Erreur SQL : {e}\nRequête : {query}")
